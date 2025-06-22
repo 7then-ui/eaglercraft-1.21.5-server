59b73d0719500436987efd7a477548386904e73a
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-06-22 10:00:11 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,12 +1,9 @@
 echo "Starting limbo..."
 sleep 1
-cd limbo && java -jar server.jar
-cd ../
+cd limbo && java -jar server.jar &
+echo "Starting velocity..."
+sleep 1
+cd velocity && java -jar server.jar &
 echo "Starting server..."
 sleep 1
 cd server && java -jar server.jar
-cd ../
-echo "Starting velocity..."
-sleep 1
-cd velocity && java -jar server.jar
-cd ../
